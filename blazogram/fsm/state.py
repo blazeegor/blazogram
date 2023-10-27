6266a628d4753e63b76562f1c36ea953a50d2c9a
CodWiz <codwiz@yandex.ru>
2023-10-27 12:02:04 +0300

    Update state.py

--- blazogram/fsm/state.py
+++ blazogram/fsm/state.py
@@ -1,2 +1,10 @@
 class State:
-    pass
+    def __init__(self, name: str):
+        self.name = name
+        self.data = {}
+
+    def update_data(self, **kwargs):
+        self.data.update(kwargs)
+
+    def __str__(self):
+        return self.name
