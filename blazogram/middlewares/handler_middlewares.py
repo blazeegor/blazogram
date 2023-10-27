2fe1d1322a863385b38c2155e63e6e102b8a2af5
CodWiz <codwiz@yandex.ru>
2023-10-27 12:07:14 +0300

    Update handler_middlewares.py

--- blazogram/middlewares/handler_middlewares.py
+++ blazogram/middlewares/handler_middlewares.py
@@ -1,6 +1,5 @@
 from .base import BaseMiddleware
 
-
 class HandlerMiddlewares:
     def __init__(self, func: callable, data: dict, args: list, update, middlewares: list[BaseMiddleware]):
         self.func = func
@@ -8,16 +7,14 @@ class HandlerMiddlewares:
         self.update = update
         self.middlewares = middlewares
         self.data = data
-        self.number = 0
 
     async def __call__(self):
-        self.number += 1
-        if self.number == len(self.middlewares):
-            for key in [key for key in self.data.keys()]:
-                if key not in self.args:
-                    self.data.pop(key)
-            await self.func(self.update, **self.data)
+        for key in self.data.keys():
+            if key not in self.args:
+                del self.data[key]
+
+        await self.func(self.update, **self.data)
 
     async def start(self):
-        for middleware in self.middlewares:
+        for index, middleware in enumerate(self.middlewares):
             await middleware(handler=self, update=self.update, data=self.data)
