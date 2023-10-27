209f9a819528202ff20103741fc1bc66995da33c
CodWiz <codwiz@yandex.ru>
2023-10-27 11:53:05 +0300

    Update __init__.py

--- blazogram/types/__init__.py
+++ blazogram/types/__init__.py
@@ -1,9 +1,9 @@
-from .message import (Message)
-from .callback_query import (CallbackQuery)
-from .update import (Update)
-from .input_file import (InputFile)
-from .user import (User)
-from .chat import (Chat)
-from .photo import (PhotoSize, ChatPhoto)
-from .reply_keyboard import (ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove)
-from .inline_keyboard import (InlineKeyboardMarkup, InlineKeyboardButton)
+from .message import Message
+from .callback_query import CallbackQuery
+from .update import Update
+from .input_file import InputFile
+from .user import User
+from .chat import Chat
+from .photo import PhotoSize, ChatPhoto
+from .reply_keyboard import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from .inline_keyboard import InlineKeyboardMarkup, InlineKeyboardButton
