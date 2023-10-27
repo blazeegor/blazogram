1c15a6a07742c2ab5f7abd07bd6c2a19b3b6a236
CodWiz <codwiz@yandex.ru>
2023-10-27 12:09:36 +0300

    Update throttling.py

--- blazogram/middlewares/throttling.py
+++ blazogram/middlewares/throttling.py
@@ -4,38 +4,36 @@ from dataclasses import dataclass
 from typing import Callable, Awaitable, Any
 from datetime import datetime, timedelta
 
-
 @dataclass(frozen=True)
 class UserKey:
     chat_id: int
     user_id: int
 
-
-@dataclass
+@dataclass(frozen=True)
 class DataKey:
     date: datetime
     is_message: bool
 
-
 class ThrottlingMiddleware(BaseMiddleware):
-    def __init__(self, delay: int = 3, message: str = '❗ Не так быстро.'):
+    def __init__(self, delay: int = 3, message: str = '❗️ Не так быстро.'):
         self.delay = delay
         self.message = message
-        self.storage: dict[UserKey, DataKey] = dict()
+        self.storage: dict[UserKey, DataKey] = {}
 
     async def __call__(self,
-                       handler: callable,
+                       handler: Callable[..., Awaitable[Any]],
                        update: Message,
                        data: dict) -> Any:
         key = UserKey(chat_id=update.chat.id, user_id=update.from_user.id)
-        if key in self.storage.keys():
+        if key in self.storage:
             if datetime.now() - self.storage[key].date > timedelta(seconds=self.delay):
                 self.storage.pop(key)
                 return await handler()
             else:
-                if self.storage[key].is_message is True:
-                    self.storage[key].is_message = False
-                    return await update.answer(text=self.message)
+                data_key = self.storage[key].replace(is_message=False)
+                self.storage[key] = data_key
+                return await update.answer(text=self.message)
         else:
-            self.storage[key] = DataKey(date=datetime.now(), is_message=True)
+            data_key = DataKey(date=datetime.now(), is_message=True)
+            self.storage[key] = data_key
             return await handler()
