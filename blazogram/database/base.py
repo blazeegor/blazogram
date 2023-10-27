6272e830cb0da85275bd1e1e165d892dae3ed348
CodWiz <codwiz@yandex.ru>
2023-10-27 11:43:00 +0300

    Update base.py

--- blazogram/database/base.py
+++ blazogram/database/base.py
@@ -1,11 +1,10 @@
 from abc import ABC, abstractmethod
 
-
 class Database(ABC):
     @abstractmethod
-    async def add_user(self, user):
+    async def add_user(self, user: dict) -> None:
         pass
 
     @abstractmethod
-    async def get_users(self) -> list:
+    async def get_users(self) -> list[dict]:
         pass
