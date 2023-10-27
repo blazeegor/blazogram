d0461082db938329dde851aed21d107ba141b3e9
CodWiz <codwiz@yandex.ru>
2023-10-27 11:47:07 +0300

    Update text.py

--- blazogram/filters/text.py
+++ blazogram/filters/text.py
@@ -5,17 +5,19 @@ from ..exceptions import FilterError
 
 class Text(BaseFilter):
     def __init__(self, text: str = None, startswith: str = None, endswith: str = None):
-        if text and startswith is None and endswith is None or startswith and text is None or endswith and text is None:
-            self.text = text
-            self.startswith = startswith
-            self.endswith = endswith
-        else:
-            raise FilterError(message='Filter Text most have a one argument.')
+        if text and (startswith or endswith):
+            raise FilterError('Filter Text should have only one argument.')
+        
+        self.text = text
+        self.startswith = startswith
+        self.endswith = endswith
 
     async def __check__(self, message: Message) -> bool:
         if self.text is not None:
             return message.text == self.text
         elif self.startswith is not None:
             return message.text.startswith(self.startswith)
-        else:
+        elif self.endswith is not None:
             return message.text.endswith(self.endswith)
+        else:
+            return False
