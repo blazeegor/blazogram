4a7db2ca835b256686404cd29bb5a62f022e7ca4
CodWiz <codwiz@yandex.ru>
2023-10-27 11:43:20 +0300

    Update memory.py

--- blazogram/database/memory.py
+++ blazogram/database/memory.py
@@ -1,18 +1,16 @@
 from .base import Database
 from ..types.user import User
 
-
 class MemoryDatabase(Database):
     def __init__(self):
         self.users: list[User] = []
 
-    async def user_exist(self, user: User) -> bool:
-        users = await self.get_users()
-        return user in users
+    async def user_exists(self, user: User) -> bool:
+        return user in self.users
 
-    async def add_user(self, user: User):
-        if await self.user_exist(user=user) is False:
+    async def add_user(self, user: User) -> None:
+        if not await self.user_exists(user):
             self.users.append(user)
 
     async def get_users(self) -> list[User]:
-        return self.users
+        return self.users.copy()
