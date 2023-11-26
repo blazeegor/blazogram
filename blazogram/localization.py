6f9b47ecd311d4d0d97d518bf9e31e9b56093db6
Peter Stone <rekiiky@proton.me>
2023-11-26 20:30:20 +0300

    Update

--- blazogram/localization.py
+++ blazogram/localization.py
@@ -1,31 +1,24 @@
-from googletrans import Translator
+from aiotrans import Translaitor
 from .enums import Languages
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class UserLanguage:
-    user_id: int
-    language: str
 
 
 class BlazeLocale:
     def __init__(self):
-        self.users: dict[int, UserLanguage] = {}
-        self.translator = Translator()
+        self.users: dict[int, str] = dict()
+        self.translator = Translaitor()
 
     def set_language(self, user_id: int, language: Languages) -> None:
-        self.users[user_id] = UserLanguage(user_id, language.name)
+        self.users[user_id] = language.name
 
     def get_language(self, user_id: int) -> str:
-        return self.users[user_id].language
+        return self.users[user_id]
 
     def check_user(self, user_id: int) -> bool:
         return user_id in self.users
 
-    def translate(self, user_id: int, text: str) -> str:
-        translation = self.translator.translate(
-            text, dest=self.get_language(user_id)
-        ).text
+    async def translate(self, user_id: int, text: str, source: Languages) -> str:
+        translation = await self.translator.translate(
+            text, target=self.get_language(user_id), source=source.name
+        )
 
         return translation
